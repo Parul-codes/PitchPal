8e5c6131bc1ee4bcd1151c36e74a3164e32e6696
Parul-codes <jainparul301@gmail.com>
2025-10-07 14:55:08 +0530

    initialized backend

---
+++ backend/src/server.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+// mongoose
+//   .connect(process.env.MONGO_URI || "")
+//   .then(() => console.log("✅ MongoDB connected"))
+//   .catch((err) => console.log("❌ MongoDB connection failed:", err));
+
+// Basic route
+app.get("/", (req, res) => {
+  res.send("PitchPal backend is running 🚀");
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port https://localhost:${PORT}`);
+});
