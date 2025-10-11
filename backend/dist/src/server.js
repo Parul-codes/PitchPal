24d6503c6edebc56d70345dd73cf6ec10fc54952
Parul-codes <jainparul301@gmail.com>
2025-10-11 21:21:36 +0530

    frontend url

--- backend/dist/src/server.js
+++ backend/dist/src/server.js
@@ -17,7 +17,7 @@ dotenv_1.default.config();
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use((0, cors_1.default)({ origin: "http://localhost:5173" }));
+app.use((0, cors_1.default)({ origin: "https://pitch-pal-omega.vercel.app" }));
 app.use(express_1.default.json());
 // MongoDB Connection
 mongoose_1.default
